9b662426ced7c0611b334e23d0ea589ad2a3c5cf
Vitória Nazareth <vitorianazareths@gmail.com>
2024-12-09 21:29:11 -0300

    up -> fix error handling + min cases handling

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
 
 #gera a lista de números até n
 def generate_list(n):
@@ -9,44 +11,46 @@ def generate_list(n):
     fizzbuzz_list.append(i)
   return fizzbuzz_list
 
+
 #função fizzbuzz principal
 def fizzBuzz(n):
-  try:
-    num = int(n)
-    if num < 0:
-      raise Exception("Seu valor precisa ser positivo")
-      
-  except ValueError:
-    print("Insira um valor inteiro positivo")
 
-  else:
-    fb_list = generate_list(n)
+  fb_list = generate_list(n)
 
-    for j in range(len(fb_list)):
-      if fb_list[j] % 3 == 0 and fb_list[j] % 5 == 0:
-        fb_list[j] = "FizzBuzz"
+  for j in range(len(fb_list)):
+    if fb_list[j] % 3 == 0 and fb_list[j] % 5 == 0:
+      fb_list[j] = "FizzBuzz"
 
-      elif fb_list[j] % 3 == 0:
-        fb_list[j] = "Fizz"
+    elif fb_list[j] % 3 == 0:
+      fb_list[j] = "Fizz"
 
-      elif fb_list[j] % 5 == 0:
-        fb_list[j] = "Buzz"
+    elif fb_list[j] % 5 == 0:
+      fb_list[j] = "Buzz"
 
-      else:
-        fb_list[j] = fb_list[j]
+    else:
+      fb_list[j] = fb_list[j]
 
   return fb_list
 
 #rota da API
 @app.route('/fizzbuzz', methods=['POST'])
 def fb_api():
+
   valor = request.get_json()
   n = valor.get("n")
-  if not n:
-    raise Exception("Um valor inteiro e positivo é necessário")
-  
-  fb_list = fizzBuzz(n)
+
+  try:
+    num = int(n)
+
+    if num < 0:
+      raise ValueError("Seu valor precisa ser um numero POSITIVO")
+    elif num == 0:
+      return jsonify([])
+    
+  except ValueError:
+    return {"erro":"Seu valor precisa ser um NUMERO positivo"}
+
+  else:
+    fb_list = fizzBuzz(n)
+
   return jsonify(fb_list)
-
-
-fizzBuzz(15)
